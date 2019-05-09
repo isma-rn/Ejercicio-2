4c29f34e306be2b138b166155e6882f76cf8ca3c
isma-rn <50065806+isma-rn@users.noreply.github.com>
2019-05-09 10:10:26 -0500

    Inicio

---
+++ E2_TablaMultiplicarHasta20/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_TablaMultiplicarHasta20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese número: ");
+            int numero = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}", ObtenerTabla( 1, 20, numero));
+
+            Console.ReadLine();
+        }
+
+        public static string ObtenerTabla(int n1, int n2, int num)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            if (n1 < n2)            
+                for (int i = n1; i <= n2; i++)                
+                    resultado.Append( string.Format( "{0} x {1} = {2}\n", num, i, (num * i) ) );
+            else
+                resultado.Append("Error");
+
+            return resultado.ToString();
+        }
+    }
+}
